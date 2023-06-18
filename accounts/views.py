53f9832b80c66e60740eac5514e1ec90f45fd725
HoshinoMone <m.hoshino.23.4.870@4nonome.com>
2023-06-18 17:23:09 +0900

    SignUp

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,5 @@
-# from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-
 from django.views.generic import CreateView
-
 from django.urls import reverse_lazy
 
 from .forms import SignupForm
