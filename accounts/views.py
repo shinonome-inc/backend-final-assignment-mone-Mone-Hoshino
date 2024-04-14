4426224715133a9739361d7aa9da340a7e1635fd
HoshinoMone <m.hoshino.23.4.870@4nonome.com>
2024-04-14 11:52:02 +0900

    ログイン機能

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import authenticate, login
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import SignupForm
 
@@ -19,3 +20,5 @@ class SignupView(CreateView):
         login(self.request, user)
         return response
         
+class UserProfileView(LoginRequiredMixin, TemplateView):
+    template_name = "accounts/profile.html"
